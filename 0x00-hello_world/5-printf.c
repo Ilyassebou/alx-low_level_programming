afe31e2f7e78277a8cdd16acd1da98286116137e
ilyassebou <bouaarrouda.ilyasse@gmail.com>
2023-09-07 07:24:06 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Use the printf function to print the specified message
+*
+* Return: Always 0 (Success)
+*/
+
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
