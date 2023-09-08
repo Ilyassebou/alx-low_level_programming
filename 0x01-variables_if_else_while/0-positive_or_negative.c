1bf042153a7633f7b1e403181f50d2b9a51761c9
ilyassebou <bouaarrouda.ilyasse@gmail.com>
2023-09-08 06:23:44 -0700

    Positive, Negative, Zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,11 +18,11 @@ int main(void)
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\ni", n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("%d is positive", n);
+		printf("%d is positive\n", n);
 	}
 
 	return (0);
