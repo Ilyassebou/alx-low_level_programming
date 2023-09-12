30084cd80d6964a29ec258fb2d743aa1078d4da7
ilyassebou <bouaarrouda.ilyasse@gmail.com>
2023-09-12 13:35:43 -0700

    98 Battery Street

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,33 +8,21 @@
  */
 void print_to_98(int n)
 {
+	int i;
+
 	if (n <= 98)
 	{
-		for (int i = n; i <= 98; i++)
+		for (i = n; i < 98; i++)
 		{
-			printf("%d", i);
-			if (i < 98)
-			{
-				printf(", ");
-			} else
-			{
-				printf("\n");
-			}
+			printf("%d, ", i);
 		}
 	}
 	else
 	{
-		for (int i = n; i >= 98; i--)
+		for (i = n; i > 98; i--)
 		{
-			printf("%d", i);
-			if (i > 98)
-			{
-				printf(", ");
-			}
-			else
-			{
-				printf("\n");
-			}
+			printf("%d, ", i);
 		}
 	}
+		printf("%d\n", i);
 }
