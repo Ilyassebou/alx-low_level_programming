1cafaf648456321013d62faeed1428c4b19278db
ilyassebou <bouaarrouda.ilyasse@gmail.com>
2023-09-19 02:16:26 -0700

    Finished

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,22 +11,26 @@
 
 int main(void)
 {
-	char password[12];
-	int sum = 0;
-	int i;
+	int pass[100];
+	int i, sum, n;
+
+	sum = 0;
 
 	srand(time(NULL));
 
-	for (i = 0; i < 11; i++)
+	for (i = 0; i < 100; i++)
 	{
-		password[i] = rand() % 94 + 33;
-		sum += password[i];
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
 	}
-
-	password[i] = 126 - (sum % 94);
-	password[12] = '\0';
-
-	printf("%s\n", password);
 
 	return (0);
 }
