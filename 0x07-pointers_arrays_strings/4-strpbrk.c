f5bbd7e537b5f1342333c9d17f922c9aaa14648b
ilyassebou <bouaarrouda.ilyasse@gmail.com>
2023-09-25 08:18:52 -0700

    Finished

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s)
+	{
+		int i;
+
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+
+	return ('\0');
+}
