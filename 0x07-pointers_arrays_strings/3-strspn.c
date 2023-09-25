f9d6dcf74688c3de0630ececb4f7310e167c8dea
ilyassebou <bouaarrouda.ilyasse@gmail.com>
2023-09-25 06:26:31 -0700

    Finished

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,24 +10,20 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int count = 0;
-	int found;
 	int i;
 
 	while (*s)
 	{
-		found = 0;
 		for (i = 0; accept[i]; i++)
 		{
 			if (*s == accept[i])
 			{
 				count++;
-				found = 1;
 				break;
 			}
+			else if (accept[i + 1] == '\0')
+				return (count);
 		}
-
-		if (!found)
-			break;
 
 		s++;
 	}
