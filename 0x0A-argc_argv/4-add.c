2fd0c53d2b2f74b5dd8ea00e48ed3bc644a76daa
ilyassebou <bouaarrouda.ilyasse@gmail.com>
2023-10-02 18:36:53 -0700

    Finished

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,44 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <ctype.h>
-#include "holberton.h"
 
 /**
- * main - Program that takes in all integer arguments and returns the sum
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
+ * main - Entry point of the program
+ *
+ * check_num - check - string there are digit
+ *
+ * @str: input string
+ *
+ * Return: 1 if all characters in the string are digits, 0 otherwise
  */
+
+int check_num(const char *str)
+{
+	while (*str)
+	{
+		if (!isdigit(*str))
+		{
+			return (0);
+		}
+		str++;
+	}
+	return (1);
+}
 
 int main(int argc, char *argv[])
 {
-	int i, j, length, sum;
-	char *ptr;
+	int sum = 0;
+	int i;
+	int num;
 
-	if (argc < 2)
-		printf("0\n");
-	else
+	for (i = 1; i < argc; i++)
 	{
-		sum = 0;
-		for (i = 1; i < argc; i++)
+		if (check_num(argv[i]))
 		{
-			ptr = argv[i];
-			length = strlen(ptr);
-
-			for (j = 0; j < length; j++)
-			{
-				if (isdigit(*(ptr + j)) == 0)
-				{
-					printf("Error\n");
-					return (1);
-				}
-			}
-
-			sum += atoi(argv[i]);
+			num = atoi(argv[i]);
+			sum += num;
 		}
-
-	printf("%d\n", sum);
+		else
+		{
+			printf("Error: Argument '%s' is not a valid integer.\n", argv[i]);
+			return (1);
+		}
 	}
+
+	printf("Sum: %d\n", sum);
 	return (0);
 }
