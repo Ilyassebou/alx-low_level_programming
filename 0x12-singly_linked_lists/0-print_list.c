e2a023f8454c2af26e8ce506812e0f9f811d1244
ilyassebou <bouaarrouda.ilyasse@gmail.com>
2023-10-19 11:09:14 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,10 +15,11 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (!h->str)
-			printf("[0] (nil)\n");
-		else
+		if (h->str)
 			printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+
 		h = h->next;
 		c++;
 	}
