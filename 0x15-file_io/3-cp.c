34596f7b6f2360186bac41157f403c40a41ed2bf
ilyassebou <bouaarrouda.ilyasse@gmail.com>
2023-10-30 15:40:05 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,59 +2,76 @@
 #include <stdio.h>
 
 /**
- * error_exit - Handle errors and exit with a specific code and message.
+ * error_file - Checks whether the specified files can be opened.
  *
- * @code: The exit code.
- * @message: The error message.
+ * @file_from: The file to read from.
+ * @file_to: The file to write to.
+ * @argv: The arguments vector.
+ *
+ * Return: no return.
  */
 
-void error_exit(int code, const char *message)
+void error_file(int file_from, int file_to, char *argv[])
 {
-	dprintf(STDERR_FILENO, "%s\n", message);
-	exit(code);
+	int exit_code = file_from == -1 ? 98 : (file_to == -1 ? 99 : 0);
+
+	if (exit_code != 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't %s %s\n",
+				exit_code == 98 ? "read from" : "write to",
+				argv[exit_code == 98 ? 1 : 2]);
+		exit(exit_code);
+	}
 }
 
 /**
- * main - Entry point for the program.
+ * main - Entry point for Holberton School code checker.
  *
- * @argc: The number of command-line arguments.
- * @argv: An array of command-line argument strings.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line argument strings.
  *
- * Return: 0 on success, or the corresponding error code on failure.
+ * Return: Always returns 0.
  */
 
 int main(int argc, char *argv[])
 {
-	int file_from, file_to;
-	ssize_t nchars, bytes_written;
+	int file_from, file_to, close_error;
+	ssize_t num_chars, num_written;
 	char buffer[1024];
 
 	if (argc != 3)
-		error_exit(97, "Usage: cp file_from file_to");
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
 
 	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
 
-	if (file_from == -1)
-		error_exit(98, "Error: Can't read from file");
+	num_chars = 1024;
+	while (num_chars == 1024)
+	{
+		num_chars = read(file_from, buffer, 1024);
+		if (num_chars == -1)
+			error_file(-1, 0, argv);
+		num_written = write(file_to, buffer, num_chars);
+		if (num_written == -1)
+			error_file(0, -1, argv);
+	}
 
-	if (file_to == -1)
-		error_exit(99, "Error: Can't write to file");
+	close_error = close(file_from);
+	if (close_error == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
 
-	do {
-		nchars = read(file_from, buffer, 1024);
-		if (nchars == -1)
-			error_exit(98, "Error: Can't read from file");
-		bytes_written = write(file_to, buffer, nchars);
-		if (bytes_written == -1)
-			error_exit(99, "Error: Can't write to file");
-	} while (nchars == 1024);
-
-	if (close(file_from) == -1)
-		error_exit(100, "Error: Can't close file_from");
-
-	if (close(file_to) == -1)
-		error_exit(100, "Error: Can't close file_to");
-
+	close_error = close(file_to);
+	if (close_error == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
 	return (0);
 }
