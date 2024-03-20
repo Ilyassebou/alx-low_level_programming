78978000772b78240b13b6207cb6b4d1b1819193
ilyassebou <bouaarrouda.ilyasse@gmail.com>
2024-03-20 14:46:34 -0700

    Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """Defines an island perimeter measuring function"""
 
-def island_perimeter(grid):
-    if not grid:
-        return 0
-    
-    perimeter = 0
-    rows = len(grid)
-    cols = len(grid[0])
 
-    for i in range(rows):
-        for j in range(cols):
+def island_perimeter(grid):
+    """Return the perimiter of an island"""
+
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
             if grid[i][j] == 1:
-                perimeter += 4
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
-    
-    return perimeter
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
